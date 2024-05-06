3e9ce5b6ce6b16d2723a939ea272ea11fb2e79d3
Miguel Angel Morales Rubio <miguelangel.morales@centene.com>
2024-05-06 20:06:19 +0200

    urls ok

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -8,10 +8,11 @@ import { Public } from 'src/auth/auth.controller';
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
-  @Get('/:jobOfferId')
+  @Get('jobOffer/:jobOfferId')
   @ApiBearerAuth()  
   @ApiOperation({ summary: 'Competencias del candidato conectado', description: 'Devuelve el valor de las competencias del candidato conectado' })
   getCandidateCompetenceReport(@Param('jobOfferId') jobOfferId: string,  @Req() request: Request): { [key: string]: number } {
+    console.log('123')
     return {
       competence1: 10,
       competence2: 20,
@@ -23,10 +24,11 @@ export class ReportController {
     };
   }
 
-  @Get('/:jobOfferId/:candidateId')
+  @Get('candidates/:jobOfferId/:candidateId')
   @ApiBearerAuth()  
   @ApiOperation({ summary: 'Competencias del candidato de una oferta', description: 'Devuelve el valor de las competencias del candidato de una oferta del CLIENTE conectado' })
-  getClientCandidateCompetenceReport(@Param('jobOfferId') jobOfferId: string, @Param('jobOfferId') candidateId: string, @Req() request: Request): { [key: string]: number } {
+  getClientCandidateCompetenceReport(@Param('jobOfferId') jobOfferId: string, @Param('candidateId') candidateId: string, @Req() request: Request): { [key: string]: number } {
+    console.log('todos12')
     return {
       competence1: 10,
       competence2: 20,
@@ -38,10 +40,11 @@ export class ReportController {
     };
   }
 
-  @Get('candidates/:jobOfferId')  
+  @Get('joboffers/:jobOfferId')  
   @ApiBearerAuth()  
   @ApiOperation({ summary: 'Competencias de todos los candidatos apuntados a una oferta', description: 'Devuelve el valor de las competencias de TODOS los candidatos de una oferta del CLIENTE conectado' })
   getReport(@Param('jobOfferId') jobOfferId: string,  @Req() request: Request): { [key: string]: any } {
+    console.log('todos')
     return [
       {
         name: 'Juan',
@@ -209,81 +212,79 @@ export class ReportController {
   @ApiBearerAuth()  
   @ApiOperation({ summary: 'Estadísticas de uso de la plataforma de los clientes de un año', description: 'Devuelve las estadísticas del uso de la plataforma por los clientes en un año (usado por el ADMIN)' })
   getClientActivity(@Param('year') year: string,  @Req() request: Request): { [key: string]: any } {
-    return  
-    [
+    console.log('todos456');
+    return [
       {
-      "companyName": "Company 5",
-      "numberOfJobOffers": 2,
-      "month": 4,
-      "numberOfSessions": 8,
-      "numberOfCandidates": 10
+      companyName: "Company 5",
+      numberOfJobOffers: 2,
+      month: 4,
+      numberOfSessions: 8,
+      numberOfCandidates: 10
       },
       {
-      "companyName": "Company 8",
-      "numberOfJobOffers": 7,
-      "month": 4,
-      "numberOfSessions": 7,
-      "numberOfCandidates": 87
+      companyName: "Company 8",
+      numberOfJobOffers: 7,
+      month: 4,
+      numberOfSessions: 7,
+      numberOfCandidates: 87
       },
       {
-      "companyName": "Company 10",
-      "numberOfJobOffers": 9,
-      "month": 7,
-      "numberOfSessions": 8,
-      "numberOfCandidates": 43
+      companyName: "Company 10",
+      numberOfJobOffers: 9,
+      month: 7,
+      numberOfSessions: 8,
+      numberOfCandidates: 43
       },
       {
-      "companyName": "Company 4",
-      "numberOfJobOffers": 9,
-      "month": 8,
-      "numberOfSessions": 9,
-      "numberOfCandidates": 84
+      companyName: "Company 4",
+      numberOfJobOffers: 9,
+      month: 8,
+      numberOfSessions: 9,
+      numberOfCandidates: 84
       },
       {
-      "companyName": "Company 10",
-      "numberOfJobOffers": 8,
-      "month": 8,
-      "numberOfSessions": 9,
-      "numberOfCandidates": 11
+      companyName: "Company 10",
+      numberOfJobOffers: 8,
+      month: 8,
+      numberOfSessions: 9,
+      numberOfCandidates: 11
       },
       {
-      "companyName": "Company 5",
-      "numberOfJobOffers": 3,
-      "month": 12,
-      "numberOfSessions": 3,
-      "numberOfCandidates": 79
+      companyName: "Company 5",
+      numberOfJobOffers: 3,
+      month: 12,
+      numberOfSessions: 3,
+      numberOfCandidates: 79
       },
       {
-      "companyName": "Company 6",
-      "numberOfJobOffers": 1,
-      "month": 3,
-      "numberOfSessions": 10,
-      "numberOfCandidates": 70
+      companyName: "Company 6",
+      numberOfJobOffers: 1,
+      month: 3,
+      numberOfSessions: 10,
+      numberOfCandidates: 70
       },
       {
-      "companyName": "Company 7",
-      "numberOfJobOffers": 6,
-      "month": 12,
-      "numberOfSessions": 4,
-      "numberOfCandidates": 84
+      companyName: "Company 7",
+      numberOfJobOffers: 6,
+      month: 12,
+      numberOfSessions: 4,
+      numberOfCandidates: 84
       },
       {
-      "companyName": "Company 8",
-      "numberOfJobOffers": 3,
-      "month": 12,
-      "numberOfSessions": 9,
-      "numberOfCandidates": 53
+      companyName: "Company 8",
+      numberOfJobOffers: 3,
+      month: 12,
+      numberOfSessions: 9,
+      numberOfCandidates: 53
       },
       {
-      "companyName": "Company 6",
-      "numberOfJobOffers": 3,
-      "month": 3,
-      "numberOfSessions": 3,
-      "numberOfCandidates": 81
+      companyName: "Company 6",
+      numberOfJobOffers: 3,
+      month: 3,
+      numberOfSessions: 3,
+      numberOfCandidates: 81
       }
-      ]
-
-    
+    ]    
    }
       
 }
